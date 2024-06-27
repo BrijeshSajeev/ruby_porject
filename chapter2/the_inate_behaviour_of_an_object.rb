366de374a6f18a7942a3c8e0ab5fc08a0877051e
BrijeshSajeev <brijesh00300@gmail.com>
2024-06-27 16:23:16 +0000

    ADD respond_to? method in object

--- chapter2/the_inate_behaviour_of_an_object.rb
+++ chapter2/the_inate_behaviour_of_an_object.rb
@@ -1,3 +1,14 @@
 # Object methods
 
 puts Object.new.methods.sort # Object methods
+
+# respond_to?
+
+obj = Object.new
+
+if obj.respond_to? "talk"
+  obj.talk
+else
+  puts "Sorry this method is not available"
+end
+
