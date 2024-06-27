c30cd82ac99b149539bb4ef025a1bec56d8bde6c
BrijeshSajeev <brijesh00300@gmail.com>
2024-06-27 16:46:41 +0000

    Add defalut value to the agrument

--- chapter2/method_argument.rb
+++ chapter2/method_argument.rb
@@ -6,11 +6,28 @@ def obj.default_arg x
 end
 
 obj.default_arg "Brijesh"
-obj.default_arg(1,2,3,4)
+#obj.default_arg(1,2,3,4)
 
 def obj.multiple_arg *x
   puts "Multiple args"
   p x
 end
 
-obj.multiple_arg(1,2,3,4,5)
+#obj.multiple_arg(1,2,3,4,5)
+
+# Default vlaues
+
+def obj.default_val(a, b=3)
+  p a, b
+end
+
+obj.default_val(1,2)
+obj.default_val(1)
+
+def obj.arg_unleased(a, b=1, *c, d)
+  p a, b, c, d
+end
+
+obj.arg_unleased(1, 2, 3, 4, 5, 6)
+obj.arg_unleased(1, 2, 5, 6)
+
